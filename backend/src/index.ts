b098cf1970b7b6182cc9b593bf1229f91b151d0b
Eduardo <dudubor2003@gmail.com>
2026-03-18 20:07:16 -0300

    Fodase

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ import helmet from 'helmet';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || "4000";
+const port = process.env.PORT || 4000;
 
 app.use(helmet());
 app.use(cors({
